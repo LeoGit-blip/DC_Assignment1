0bc9d3e9529254d3e52420047cfc0997193a0a88
kmbrly02 <700042169@student.curtin.edu.my>
2024-08-24 22:43:56 +0800

    Done MainWindow, EnterMainWindow, and Lobby design. Left with PrivateMessage design

--- DC_Assignment/PrivateMessageWindow.xaml.cs
+++ DC_Assignment/PrivateMessageWindow.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace DC_Assignment
+namespace Client
 {
     /// <summary>
     /// Interaction logic for PrivateMessageWindow.xaml
